44b6ee963f0c24fcd7ebfb4b95fc427181cbe82d
Ramansh Shah <shahramansh@gmail.com>
2026-04-02 07:33:06 +0530

    Class model added

---
+++ backend/models/Class.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  code: { type: String, required: true, unique: true },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Class', classSchema);
